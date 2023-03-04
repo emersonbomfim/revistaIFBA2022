6a4455d7c3d3cb538d761cc4e6eb0112ad75dc0c
Emerson Bomfim <97489675+emersonbomfim@users.noreply.github.com>
2023-03-04 12:45:24 -0300

    Add files via upload

---
+++ GABRIEL BARRETO SOARES SANTOS/Site Kebrada/script.js
@@ -0,0 +1,19 @@
+ function menub(x) 
+{
+  x.classList.toggle("change");
+}
+
+var acc = document.getElementsByClassName("menub");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
