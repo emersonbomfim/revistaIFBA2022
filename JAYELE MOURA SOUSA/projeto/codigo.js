7f322b49b6666d1dd9826f3c67e88977ff469c81
Emerson Bomfim <97489675+emersonbomfim@users.noreply.github.com>
2023-03-04 12:53:02 -0300

    Add files via upload

---
+++ JAYELE MOURA SOUSA/projeto/codigo.js
@@ -0,0 +1,8 @@
+function menua(){
+					document.getElementById('menu').style.width= '250px';
+					document.getElementById('main').style.marginLeft= '250px';
+				}
+				function menuf(){
+					document.getElementById('menu').style.width= '0px';
+					document.getElementById('main').style.marginLeft= '0px';
+				}	
---
+++ JAYELE MOURA SOUSA/projeto/script.js
@@ -0,0 +1,11 @@
+function menua(){
+					document.getElementById('menu').style.width= '250px';
+					document.getElementById('main').style.marginLeft= '250px';
+				}
+				function menuf(){
+					document.getElementById('menu').style.width= '0px';
+					document.getElementById('main').style.marginLeft= '0px';
+				}	
+function cadastro(form) {
+            alert("Sua mensagem foi enviada com sucesso!");
+            }
